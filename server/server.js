8bc693279c0b358a6d6ff93d42f65df98b87b9f8
anuragpandey271299 <anurag.info27@gmail.com>
2024-01-18 15:32:02 +0530

    connected to DB and user schema is ready

--- server/server.js
+++ server/server.js
@@ -1,7 +1,12 @@
 const express=require('express')
 const dotenv=require('dotenv').config()
+const mongoose=require('mongoose')
+const user=require('./Models/user')
 const app=express()
 
 app.listen(process.env.PORT,()=>{
-    console.log('server is running on http://localhost:5000')
+    mongoose.connect(process.env.MONGODB_URL)
+    .then(()=>console.log('connected to DB'))
+    .catch((error)=>console.log(error))
+    console.log(`server is running on http://localhost:${process.env.PORT}`)
 })
